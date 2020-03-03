86f7b6d1d204fcb32408e3b8bc02ad5a8943204a
akihanari <akihanari@gmail.com>
2020-03-03 12:35:32 +0900

    first_commit

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xxxx-xxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "すみません、よくわかりません。"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
