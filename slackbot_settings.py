eb7f05cd9918934f8c6e1c8393b336c3bf8a79b9
akihanari <akihanari@gmail.com>
2020-03-07 14:10:35 +0900

    verifivation_token_add

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,8 +1,33 @@
 # coding: utf-8
 import os
+import json
+import webapp2
 # botアカウントのトークンを指定
 
-API_TOKEN = os.environ['SLACKBOT_API_TOKEN']
+class EchoHandler(webapp2.RequestHandler):
+    VERIFICATION_TOKEN = os.environ['SLACKBOT_VERIFICATION_TOKEN']
+    # API_TOKEN = os.environ['SLACKBOT_API_TOKEN']
+
+    def post(self):
+        body = json.loads(self.request.body)
+        if body['token'] != self.VERIFICATION_TOKEN:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.status = 403
+            self.response.write('403 Forbidden')
+            return
+        if body['type'] == 'url_verification':
+            self.verify_url(body)
+
+    def verify_url(self, params):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(params['challenge'])
+
+app = webapp2.WSGIApplication([
+    ('/echo-bot-hook', EchoHandler)
+], debug=True)
+
+
+# API_TOKEN = os.environ['SLACKBOT_API_TOKEN']
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "すみません、よくわかりません。"
