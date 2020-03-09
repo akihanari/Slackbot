b4644f30e85a288360ae5c2a23c5895d647a2be5
akihanari <akihanari@gmail.com>
2020-03-09 23:40:55 +0900

    fix_mention

--- plugins/fornito_mention.py
+++ plugins/fornito_mention.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import os
 import urllib
+import urllib.parse
 import json
 import datetime
 import calendar
@@ -111,7 +112,7 @@ def weather(message):
     search_word = message.body['text'].split()
     if len(search_word) == 2:
         # 追加部分
-        pref = search_word[1]  # 都道府県
+        pref = urllib.parse.quote(search_word[1])  # 都道府県
         # message.send('地域を以下から指定してください↓')
         # city = message  # 地域
         # ここまで
